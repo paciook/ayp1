0d310964b63245db87bcfd6c960e57768c7129bb
Francisco Pacio <fpaciook@gmail.com>
2021-11-22 01:53:43 -0300

    Arreglo malinterpretacion de consigna

--- TP4/main.c
+++ TP4/main.c
@@ -175,7 +175,7 @@ int main(int argc, char *argv[]) {
     }
 
     int ancho, alto;
-    if(!( (ancho = atoi(argv[1])) && (ancho > 0) && (alto = atoi(argv[2])) )){
+    if(!( (ancho = atoi(argv[1])) > 0 && (alto = atoi(argv[2])) > 0 )){
         fprintf(stderr, "Alto y/o ancho invalido/s.\n");
         return 2;
     }
